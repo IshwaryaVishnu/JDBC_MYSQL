274711b67ee26883775310137bd81f5fde9c3d11
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-12-20 14:35:07 +0100

    sql

--- src/main/java/se/lexicon/JDBC_Demo.java
+++ src/main/java/se/lexicon/JDBC_Demo.java
@@ -1,8 +1,7 @@
 package se.lexicon;
+import se.lexicon.model.Task;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBC_Demo {
 
@@ -11,6 +10,28 @@ public class JDBC_Demo {
         try {
             //Define database connection
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lecture_db","root","1246");
+            Statement statement = connection.createStatement();
+            String selectAllTasks ="select * from tasks";
+
+            // executeQuery() is used to execute SELECT query
+             ResultSet resultSet = statement.executeQuery(selectAllTasks);
+             while(resultSet.next()){
+                 System.out.println();
+                 int id = resultSet.getInt("id");
+                 String title = resultSet.getString("title");
+                 String desc = resultSet.getString("description");
+                 int personId = resultSet.getInt("person_id");
+                 Task task = new Task(id,title,desc);
+
+
+                 String firstName = resultSet.getString("p.first_name");
+                 String lastName = resultSet.getString("p.last_name");
+                 Date regDate = resultSet.getDate("p.reg_date");
+                 boolean active = resultSet.getBoolean("p._active");
+
+
+             }
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
